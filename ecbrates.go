49438e102a44f4aa57e28d05826cbe78225c41df
Igor Dolzhikov <bluesriverz@gmail.com>
2014-11-02 23:07:47 +0600

    typos in comments

--- ecbrates.go
+++ ecbrates.go
@@ -22,10 +22,10 @@ Example:
 			log.Fatal("Error: ", err)
 		}
 
-		// Variant 1: get dollar rate relative to euro
+		// Case 1: get dollar rate relative to euro
 		fmt.Println("Exchange rate", r.Date, ": EUR 1 -> USD", r.Rate[ecbrates.USD])
 
-		// Variant 2: convert of 100 euros to dollars
+		// Case 2: convert of 100 euros to dollars
 		fmt.Println(
 			"Exchange rate", r.Date,
 			": EUR 100.0 -> USD", r.Convert(100, ecbrates.EUR, ecbrates.USD),
--- example/example.go
+++ example/example.go
@@ -13,16 +13,16 @@ func main() {
 		log.Fatal("Error: ", err)
 	}
 
-	// Variant 1: get dollar rate relative to euro
+	// Case 1: get dollar rate relative to euro
 	fmt.Println("Exchange rate", r.Date, ": EUR 1 -> USD", r.Rate[ecbrates.USD])
 
-	// Variant 2: convert of 100 euros to dollars
+	// Case 2: convert of 100 euros to dollars
 	fmt.Println(
 		"Exchange rate", r.Date,
 		": EUR 100.0 -> USD", r.Convert(100, ecbrates.EUR, ecbrates.USD),
 	)
 
-	// Variant 3: convert of 100 dollars to yens
+	// Case 3: convert of 100 dollars to yens
 	fmt.Println(
 		"Exchange rate", r.Date,
 		": USD 100.0 -> JPY", r.Convert(100, ecbrates.USD, ecbrates.JPY),
